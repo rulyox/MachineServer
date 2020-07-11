7a7369f3718e5d9b4c78e6f19a0011511447444c
rulyox <email@rulyox.com>
2020-07-11 10:01:41 +0900

    Added model functions to server

---
+++ config.py
@@ -0,0 +1,2 @@
+PYTHON_PATH = './model/venv/Scripts/python.exe'
+MODEL_PATH = './model/image_classification.py'
--- server.py
+++ server.py
@@ -1,28 +1,61 @@
 from subprocess import Popen, PIPE, STDOUT
 from flask import Flask, request, jsonify
-
-PYTHON = 'python.exe'
-EXE = 'test.py'
+from config import PYTHON_PATH, MODEL_PATH
 
 # child process
-child = Popen([PYTHON, EXE], stdin=PIPE, stdout=PIPE, stderr=STDOUT)
+child = None
 
 # server app
 app = Flask(__name__)
 
 
-@app.route('/test', methods=['POST'])
-def test():
-    data = request.get_json()
-    text = data['text']
+def write_child(data):
+    global child
 
-    # send to child
-    child.stdin.write(f'{text}\n'.encode())
+    child.stdin.write(f'{data}\n'.encode())
     child.stdin.flush()
 
+
+def read_child():
+    global child
+
+    value = child.stdout.readline()
+    data = value.decode().strip()
+
+    return data
+
+
+@app.route('/train', methods=['POST'])
+def train():
+    global child
+
+    # create child process
+    command = [PYTHON_PATH, MODEL_PATH]
+    child = Popen(command, stdin=PIPE, stdout=PIPE, stderr=STDOUT)
+
     # receive from child
-    output = child.stdout.readline()
-    result = output.decode().strip()
+    while True:
+        result = read_child()
+
+        if result.startswith('accuracy:'):
+            response = {
+                'result': result
+            }
+            return jsonify(response)
+
+
+@app.route('/evaluate', methods=['POST'])
+def evaluate():
+    global child
+
+    data = request.get_json()
+    text = data['index']
+
+    # send to child
+    write_child(text)
+
+    # receive from child
+    result = read_child()
 
     response = {
         'result': result
