2c4d4f65abbb5f4a76173648c181260ff14ec830
rulyox <email@rulyox.com>
2020-07-12 14:54:28 +0900

    Check if child process is available

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ from config import PYTHON_PATH, MODEL_PATH
 
 # child process
 child = None
+is_available = False
 
 # server app
 app = Flask(__name__)
@@ -12,55 +13,85 @@ app = Flask(__name__)
 def write_child(data):
     global child
 
-    child.stdin.write(f'{data}\n'.encode())
-    child.stdin.flush()
+    if hasattr(child, 'stdin'):
+        child.stdin.write(f'{data}\n'.encode())
+        child.stdin.flush()
 
 
 def read_child():
     global child
+    global is_available
 
-    value = child.stdout.readline()
-    data = value.decode().strip()
+    if hasattr(child, 'stdout'):
+        value = child.stdout.readline()
+        data = value.decode().strip()
 
-    return data
+        return data
+
+    else:
+        disable_child()
+        raise Exception('Child process error')
+
+
+def enable_child():
+    global is_available
+
+    is_available = True
+
+
+def disable_child():
+    global is_available
+
+    is_available = True
 
 
 @app.route('/train', methods=['POST'])
 def train():
     global child
 
-    # create child process
-    command = [PYTHON_PATH, MODEL_PATH]
-    child = Popen(command, stdin=PIPE, stdout=PIPE, stderr=STDOUT)
+    try:
+        # create child process
+        command = [PYTHON_PATH, MODEL_PATH]
+        child = Popen(command, stdin=PIPE, stdout=PIPE, stderr=STDOUT)
 
-    # receive from child
-    while True:
-        result = read_child()
+        # receive from child
+        while True:
+            result = read_child()
 
-        if result.startswith('accuracy:'):
-            response = {
-                'result': result
-            }
-            return jsonify(response)
+            if result.startswith('accuracy:'):
+                enable_child()
+                response = {'result': result}
+                return jsonify(response)
+
+    except Exception as e:
+        response = {'error': str(e)}
+        return jsonify(response)
 
 
 @app.route('/evaluate', methods=['POST'])
 def evaluate():
-    global child
+    global is_available
+
+    if not is_available:
+        response = {'error': 'Child process is not available'}
+        return jsonify(response)
 
     data = request.get_json()
     text = data['index']
 
-    # send to child
-    write_child(text)
+    try:
+        # send to child
+        write_child(text)
 
-    # receive from child
-    result = read_child()
+        # receive from child
+        result = read_child()
 
-    response = {
-        'result': result
-    }
-    return jsonify(response)
+        response = {'result': result}
+        return jsonify(response)
+
+    except Exception as e:
+        response = {'error': str(e)}
+        return jsonify(response)
 
 
 if __name__ == '__main__':
